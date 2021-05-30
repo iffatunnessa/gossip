c552794b44d16c4ab39fb681e8a725572e07c216
iffatunnessa <45840519+iffatunnessa@users.noreply.github.com>
2021-05-30 22:49:48 +0600

    login css modified

--- src/component/Login/Login.js
+++ src/component/Login/Login.js
@@ -2,22 +2,23 @@ import React from 'react';
 import './Login.css'
 import {auth,provider} from '../../firebase';
 import Button from '@material-ui/core/Button';
+import gossipLogoBlack from '../../image/gossip-logo-black.png'
+import gossipLogoPurple from '../../image/logo.png'
 
 const Login = () => {
     const signIn =()=>{
     auth.signInWithPopup(provider).catch((error)=>{
         alert('error.message')
     })
-
-
     }
     return (
         <div className="main_div">
           <div className="img_div">
-              <img src="https://www.shutterstock.com/search/gossip+logo" alt="" />
+              <img className= 'logoLogin' src={gossipLogoPurple} alt="" />
+              <img src={gossipLogoBlack} alt="" />
           </div>
         <div className="child_div">
-        <Button onClick={signIn} variant="contained" color="primary">
+        <Button onClick={signIn} variant="contained" color="primary" size='large'>
          Google Sign In
         </Button>
         
